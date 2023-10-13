e1df3dbd7f306b37fc57dff339aa9b0a17be26e6
mcioc21 <126081015+mcioc21@users.noreply.github.com>
2023-10-13 10:21:55 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var http = require("http");
+const express = require("express");
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+app.use(express.static(__dirname + "/src/public"));
+
+app.get("/", (req, res) => {
+  res.send("hello.world");
+});
+
+// app.get("/server/ping", (req, res) => {
+//   res.send("pong");
+// });
+
+app.listen(8080);
